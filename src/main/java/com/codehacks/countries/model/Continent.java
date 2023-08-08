009368d2c9d470cf523bb6cc433df2df94d63424
Rume7 <electengrhume@gmail.com>
2023-08-08 15:48:50 +0100

    Added schema for continents

---
+++ src/main/java/com/codehacks/countries/model/Continent.java
@@ -0,0 +1,2 @@
+package com.codehacks.countries.model;public record Continent() {
+}
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,3 +1,10 @@
+CREATE TABLE IF NOT EXISTS Continent (
+    id INT AUTO_INCREMENT,
+    country VARCHAR(50),
+    name VARCHAR(30),
+    PRIMARY KEY(id)
+);
+
 CREATE TABLE IF NOT EXISTS Country (
     id INT AUTO_INCREMENT,
     name VARCHAR(250),
@@ -5,5 +12,7 @@ CREATE TABLE IF NOT EXISTS Country (
     emoji VARCHAR(100),
     currency VARCHAR(50),
     code VARCHAR(50),
+    phone VARCHAR(50),
+    continent_id INTEGER,
     PRIMARY KEY (id)
 );
