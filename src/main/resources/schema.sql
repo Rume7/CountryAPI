0da62fe7e0e36e529731e1372c3961928399eac7
Rume7 <electengrhume@gmail.com>
2023-08-08 14:16:00 +0100

    Schema to create h2 database table

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS Country (
+    id INT AUTO_INCREMENT,
+    name VARCHAR(250),
+    capital VARCHAR(100),
+    emoji VARCHAR(100),
+    currency VARCHAR(50),
+    code VARCHAR(50),
+    PRIMARY KEY (id)
+);
