39a193dc82f57b3421f1d1540fece39d355e45c5
Rume7 <electengrhume@gmail.com>
2023-08-08 15:49:18 +0100

    Created a Continent class

--- src/main/java/com/codehacks/countries/model/Continent.java
+++ src/main/java/com/codehacks/countries/model/Continent.java
@@ -1,2 +1,45 @@
-package com.codehacks.countries.model;public record Continent() {
+package com.codehacks.countries.model;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
+import java.util.Objects;
+
+@JsonSerialize
+@JsonDeserialize
+public class Continent {
+
+    private String name;
+
+    public Continent() {}
+
+    public Continent(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Continent continent = (Continent) o;
+        return name.equals(continent.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
+    @Override
+    public String toString() {
+        return "name=" + getName() ;
+    }
 }
