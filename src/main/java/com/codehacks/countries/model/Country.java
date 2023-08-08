7b2313e87f6ccd38d4cc1caca81b4d6e2d17de4a
Rume7 <electengrhume@gmail.com>
2023-08-08 15:50:08 +0100

    Added phone and continent field to the Country record.

--- src/main/java/com/codehacks/countries/model/Country.java
+++ src/main/java/com/codehacks/countries/model/Country.java
@@ -1,7 +1,14 @@
 package com.codehacks.countries.model;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import org.springframework.data.annotation.Id;
 
+import java.io.Serializable;
+import java.util.List;
+
+@JsonSerialize
+@JsonDeserialize
 public record Country (
         @Id
         Integer id,
@@ -9,6 +16,8 @@ public record Country (
         String capital,
         String emoji,
         String currency,
-        String code
-){
+        String code,
+        String phone,
+        Continent continent
+) {
 }
