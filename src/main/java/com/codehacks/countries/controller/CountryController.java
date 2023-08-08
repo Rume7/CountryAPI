68e634eb91d5e0e08b27bd94746eac2be7fd7f23
Rume7 <electengrhume@gmail.com>
2023-08-08 14:13:31 +0100

    Controller class to handle REST endpoints

---
+++ src/main/java/com/codehacks/countries/controller/CountryController.java
@@ -0,0 +1,41 @@
+package com.codehacks.countries.controller;
+
+import com.codehacks.countries.model.Country;
+import com.codehacks.countries.service.CountryService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/countries")
+public class CountryController {
+
+    private final CountryService countryService;
+
+    public CountryController(CountryService countryService) {
+        this.countryService = countryService;
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<?> getAllCountries() {
+        Mono<List<Country>> countries = this.countryService.getCountries();
+        if (countries.hasElement().block()) {
+            return new ResponseEntity<>(countries, HttpStatus.FOUND);
+        }
+        return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
+    }
+
+    @GetMapping("/nameOfCountry")
+    public ResponseEntity<?> getCountry(@PathVariable("nameOfCountry") String country) {
+
+        Mono<List<Country>> countryList = countryService.searchCountry(country);
+        return new ResponseEntity<>(countryList, HttpStatus.FOUND);
+    }
+}
